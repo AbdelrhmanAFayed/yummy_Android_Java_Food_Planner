83b1c09ffe36e20eb6bdbdd3636ef91a1c810c19
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-04-30 23:17:31 +0300

    Started Working on Search Fragments

--- yummy/app/src/main/java/com/example/yummy/main/view/fragments/SearchFragment.java
+++ yummy/app/src/main/java/com/example/yummy/main/view/fragments/SearchFragment.java
@@ -2,13 +2,18 @@ package com.example.yummy.main.view.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.example.yummy.R;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class SearchFragment extends Fragment {
@@ -24,5 +29,20 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Meals"));
+        tabLayout.addTab(tabLayout.newTab().setText("Categories"));
+        tabLayout.addTab(tabLayout.newTab().setText("Countries"));
+        tabLayout.addTab(tabLayout.newTab().setText("Ingredients"));
+
     }
 }
