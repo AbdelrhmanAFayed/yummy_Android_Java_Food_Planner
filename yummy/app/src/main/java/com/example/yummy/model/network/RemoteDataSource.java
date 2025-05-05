d3e58a24f90fe60211664aeb68a9d37e018383b6
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-05-05 18:18:49 +0300

    Removed Useless String

--- yummy/app/src/main/java/com/example/yummy/model/network/RemoteDataSource.java
+++ yummy/app/src/main/java/com/example/yummy/model/network/RemoteDataSource.java
@@ -6,7 +6,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 abstract public class RemoteDataSource {
 
     public static final String API_BASE_URL = "https://themealdb.com/api/" ;
-    public static final String Images_BASE_URL = "https://themealdb.com/images/";
 
     private static Retrofit retrofit = null ;
 
