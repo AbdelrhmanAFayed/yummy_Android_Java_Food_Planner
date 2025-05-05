b9cf1ca3254f4b851b942d48a6928d532e6704b1
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-05-05 18:09:14 +0300

    Removed Useless Modifier

--- yummy/app/src/main/java/com/example/yummy/model/mealshort/MealShortRepository.java
+++ yummy/app/src/main/java/com/example/yummy/model/mealshort/MealShortRepository.java
@@ -4,9 +4,9 @@ import com.example.yummy.model.network.mealshort.MealShortNetWorkCallBack;
 
 public interface MealShortRepository {
 
-    public void getByCat(String cat , MealShortNetWorkCallBack callBack);
+    void getByCat(String cat , MealShortNetWorkCallBack callBack);
 
-    public void getByCountry(String country, MealShortNetWorkCallBack callBack);
+    void getByCountry(String country, MealShortNetWorkCallBack callBack);
 
-    public void getByIng(String ing , MealShortNetWorkCallBack callBack );
+    void getByIng(String ing , MealShortNetWorkCallBack callBack );
 }
