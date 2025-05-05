dea591a3e4136306222d91a8ba9bd651eca451ca
AbdelrhmanAFayed <abdelrhmanfayed2002@gmail.com>
2025-05-05 18:45:46 +0300

    Cleaning main Activity

--- yummy/app/src/main/java/com/example/yummy/main/view/MainActivity.java
+++ yummy/app/src/main/java/com/example/yummy/main/view/MainActivity.java
@@ -5,19 +5,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.yummy.R;
-import com.example.yummy.details.view.DetailedMeal;
 import com.example.yummy.onboarding.view.OnBoarding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.yummy.databinding.ActivityMainBinding;
